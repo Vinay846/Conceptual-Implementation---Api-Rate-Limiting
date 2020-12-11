71c53d247c76484111c7163ab3cec846978d6f26
Vinay846 <Vinayvin84629@gmail.com>
2020-12-11 17:57:11 +0530

    Answer

--- src/index.js
+++ src/index.js
@@ -15,21 +15,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
 
-const isNullOrUndefined = val => val === null || val === undefined;
-
 let reqCount = 0;
 const handleReq = [];
 
 app.get("/api/posts", (req, res)=>{
 
     let max = parseInt(req.query.max);
+    console.log(max);
     let toSend = [];
     reqCount++;
     
     if(reqCount > 5){
         res.status(429).send({message: "Exceed Number of API Calls"});
     }else{
-        if(isNullOrUndefined(max) || max > 20){
+        if(isNaN(max) || max > 20){
             max = 10;
         }
     
